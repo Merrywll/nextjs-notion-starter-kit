e96b3368ef1946a74edf12c43d63bd52e9de6db8
Merrywll <88426624+Merrywll@users.noreply.github.com>
2021-08-04 16:14:34 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'f4b11c3bd6d84b429dcc829a35ce1b57',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '灵魂日志',
+  domain: 'https://verbose-eel-2fd.notion.site/f4b11c3bd6d84b429dcc829a35ce1b57',
+  author: 'CHNMerry',
 
   // open graph metadata (optional)
   description: 'Example site description',
